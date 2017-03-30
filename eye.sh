5da1c441d63883a54190b204355ba0a8f9becfef
Joel James <joelj@qburst.com>
2017-03-30 18:16:59 +0530

    Alert message change

--- eye.sh
+++ eye.sh
@@ -2,6 +2,6 @@
 export DISPLAY=:0
 zenity \
 --info \
---text="<span size=\"xx-large\">Take a break!</span>\n\nGive your eyes a <b>20 seconds</b> break." \
+--text="<span size=\"xx-large\">Take a break!</span>\n\nGive your eyes a <b>20 seconds</b> break and focus your eyes on something at least <b>20 feet</b> away." \
 --title="Eye Strain - Relief" \
 --ok-label="Sure"
